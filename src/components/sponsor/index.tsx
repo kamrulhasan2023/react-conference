50f991e1a66941bcfce0f470bc8f284184f4b67a
kamrul373 <kamrul@lemonhive.com>
2023-07-17 00:06:09 +0600

    sponsors module updated

--- src/components/sponsor/index.tsx
+++ src/components/sponsor/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ISponsor } from './interface';
-
+import { CustomImage } from '@components/custom-image';
 const Sponsor = ({sponsorType,sponsorsLogo}: ISponsor) => {
 	const sponsorTypeLogo = sponsorType === "Gold" ? "🥇" : sponsorType === "Silver" ? "🥈" : "🥉";
 	return (
@@ -8,10 +8,13 @@ const Sponsor = ({sponsorType,sponsorsLogo}: ISponsor) => {
 			<h5 className='text-center lg:mb-8 mb-4 text-[#667085] font-medium text-[12px] lg:text-base' >
 				{sponsorTypeLogo}{sponsorType} Sponsors
 			</h5>
-			<div className='flex justify-center gap-14 flex-wrap mb-8'>
+			<div className='flex justify-center items-center flex-wrap  gap-14  mb-8 '>
 				{
 					sponsorsLogo.map((sponsor,idx) => {
-						return  <img key={idx} src={sponsor.logo} alt="logo" />
+						return  <div key={idx} className='md:w-auto w-[107px]'>
+							{/* <img  src={sponsor.logo} alt="logo"  /> */}
+							<CustomImage src={sponsor.logo} alt="logo" width={290} height={65} />
+						</div>
 					} )
 				}
 			</div>
