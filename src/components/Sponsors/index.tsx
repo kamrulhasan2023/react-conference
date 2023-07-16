603c99a3f8240f9418206d21f5dd521ad68ac4ee
kamrul373 <kamrul@lemonhive.com>
2023-07-16 20:54:18 +0600

    update

--- src/components/Sponsors/index.tsx
+++ src/components/Sponsors/index.tsx
@@ -5,7 +5,7 @@ import Sponsor from '@components/sponsor';
 const Sponsors = ({title, sponsors}:ISponsors) => {
 	return (
 		<div className='w-full bg-[#F9FAFB] py-14 lg:py-[100px]'>
-			<h2 className='lg:text-[48px] text-[20px] font-bold text-center text-[#0A142F]'>{title}</h2>
+			<h2 className='lg:text-[48px] text-[20px] font-bold text-center text-[#0A142F] lg:mb-8 mb-6'>{title}</h2>
 			{
 				sponsors.map((sponsor,idx) => <Sponsor key={idx} {...sponsor} />)
 			}
--- src/components/sponsor/index.tsx
+++ src/components/sponsor/index.tsx
@@ -4,11 +4,11 @@ import { ISponsor } from './interface';
 const Sponsor = ({sponsorType,sponsorsLogo}: ISponsor) => {
 	const sponsorTypeLogo = sponsorType === "Gold" ? "🥇" : sponsorType === "Silver" ? "🥈" : "🥉";
 	return (
-		<div>
-			<h5 className='text-center my-8'>
+		<div className='px-8'>
+			<h5 className='text-center lg:mb-8 mb-4 text-[#667085] font-medium text-[12px] lg:text-base' >
 				{sponsorTypeLogo}{sponsorType} Sponsors
 			</h5>
-			<div className='flex justify-center gap-14 flex-wrap'>
+			<div className='flex justify-center gap-14 flex-wrap mb-8'>
 				{
 					sponsorsLogo.map((sponsor,idx) => {
 						return  <img key={idx} src={sponsor.logo} alt="logo" />
