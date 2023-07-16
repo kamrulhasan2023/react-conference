f9f773928740ccc38d1e37445425cefb395e6308
kamrul373 <kamrul@lemonhive.com>
2023-07-17 00:14:00 +0600

    initialized

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -2,7 +2,7 @@ import Container from '@components/container';
 
 export default function Footer() {
   return (
-    <footer role="presentation" className="bg-black-light py-6 lg:py-8">
+    <footer role="presentation" className="bg-[#111D5E] text-light py-6 lg:py-8">
       <Container>
         <p className="text-center text-base text-white">
           &copy; Lemon Hive. {new Date().getFullYear()}
