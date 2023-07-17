888dbdf3f0ba5888ddcbbd15fa84e5e78c573d28
kamrul373 <kamrul@lemonhive.com>
2023-07-17 17:37:31 +0600

    footer base, style added

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ let nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['cdn.sanity.io', 'assets.imgix.net', 'res.cloudinary.com'],
+    domains: ['cdn.sanity.io', 'assets.imgix.net', 'res.cloudinary.com', 'i.ibb.co'],
   },
 };
 
--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -1,12 +1,23 @@
 import Container from '@components/container';
-
+import Image from 'next/image';
+import logo from "./assets/img/footerLogo.png";
+import { FaTwitter, FaFacebook, FaLinkedin, FaDribbble } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer role="presentation" className="bg-[#111D5E] text-light py-6 lg:py-8">
       <Container>
-        <p className="text-center text-base text-white">
-          &copy; Lemon Hive. {new Date().getFullYear()}
-        </p>
+		<div className='flex flex-col justify-center items-center md:py-9 py-4 text-white'>
+			<Image src={logo} width={175} height={48} alt="logo" />
+			<div className='flex justify-center items-center gap-8 md:mt-[78px] mt-12 mb-6 text-xl'>
+				<FaTwitter/>
+				<FaLinkedin />
+				<FaFacebook />
+				<FaDribbble />
+			</div>
+			<div className='leading-6'>
+				<span>&copy; 2023 Lemonhive. All rights reserved.</span>
+			</div>
+		</div>
       </Container>
     </footer>
   );
