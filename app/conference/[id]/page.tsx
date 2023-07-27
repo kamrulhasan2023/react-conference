691bbbfc715200d43f6832a624dfe886e14fbf54
kamrulhasan2023 <kamrul@lemonhive.com>
2023-07-27 14:18:26 +0600

    updated

--- app/conference/[id]/page.tsx
+++ app/conference/[id]/page.tsx
@@ -1,9 +1,11 @@
+import SingleConferenceDetails from '@components/single-conference-details';
 import React from 'react';
 
 const singleEventPage = () => {
     return (
         <div>
             <h2>SIngle event page</h2>
+            <SingleConferenceDetails />
         </div>
     );
 };
--- src/components/single-conference-details/index.tsx
+++ src/components/single-conference-details/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import styles from "./single.module.scss";
 
 const SingleConferenceDetails = () => {
     return (
         <div>
-            
+            <div className={`${styles['details-container']}`}>
+                <div className="list-item">
+                    <div className="item-content">
+                        <span className="order">1</span> Alpha
+                    </div>
+                </div>
+
+                <div className="list-item">
+                    <div className="item-content">
+                        <span className="order">2</span> Bravo
+                    </div>
+                </div>
+
+                <div className="list-item">
+                    <div className="item-content">
+                        <span className="order">3</span> Charlie
+                    </div>
+                </div>
+
+                <div className="list-item">
+                    <div className="item-content">
+                        <span className="order">4</span> Delta
+                    </div>
+                </div>
+            </div>
+            <div className="circle">+</div>
         </div>
     );
 };
